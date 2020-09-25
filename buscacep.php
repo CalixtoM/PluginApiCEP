3da8d56ea4a177519b9a93d0aea24a3fb0b3b837
Matheus Calixto <mcalixto2003@gmail.com>
2020-09-25 08:38:31 -0300

    Primeiro commit plugin

---
+++ buscacep.php
@@ -0,0 +1,16 @@
+<?php
+/**
+    * Plugin Name:  Busca pelo CEP
+    * Description: Plugin que busca dados de endereço de alguém pelo CEP
+    * Version: 1.0
+    * Author: Matheus Calixto
+    * Author URI: https://www.joguinhoseletronicos.com.br
+ */
+
+function(busca_cep){
+    return '<h3>teste cep</h3>'
+}
+
+add_shortcode('buscacep', 'busca_cep');
+
+ ?>
